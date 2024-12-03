f2ea4c8b5c51ba7946bd42dec02f62cb02cf3042
rflperry <rflperry@biostat-bayes0.biostat.washington.edu>
2024-12-03 12:27:09 -0800

    elbow figures

--- make_figures.sh
+++ make_figures.sh
@@ -48,3 +48,18 @@
 # Rscript Figure4_confidence_interval_widths.R \
 #     data/sim_conf_ints_alpha=0.1_c=1_choi=FALSE_m=1_method=zg_mle=TRUE_n=100_p=50_rank=5_reps=1000_signal_alpha_frac=0.75.RData \
 #     --sigmas 0.1 0.2
+
+# #
+# #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# # 	ZG Rule
+# #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# #
+
+# # # CI Widths: regular
+Rscript Figure4_confidence_interval_widths.R \
+    data/sim_conf_ints_alpha=0.1_c=1_m=1_method=elbow_n=50_p=10_rank=5_reps=1000_signal_alpha_frac=0.75.RData \
+    --sigmas 0.15 0.25
+
+# Power: regular
+Rscript Figure5_power.R \
+    data/sim_hypo_tests_alpha=0.1_m=1_method=elbow_n=50_p=10_rank=5_reps=1000.RData
